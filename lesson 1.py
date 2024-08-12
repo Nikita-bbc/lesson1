a0c1089fd79e7f592b7e12e1a165db51d75e883e
Nikita-bbc <nikn08.58@gmail.com>
2024-08-12 20:23:01 +0300

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,10 @@
+# 1st program
+print((9 ** 0.5) * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000.0 != 1000.1)
+# 3rd program
+print(2 + 2 * 2 == (2 + 2) * 2)
+# 4th program
+n = 123.456
+t = int(n * 10)
+print(t % 10)
