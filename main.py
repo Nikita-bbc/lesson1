c45f31dedad1fae16ef10bd71bac264f8df2d693
Nikita-bbc <nikn08.58@gmail.com>
2024-08-12 17:07:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print((9 ** 0.5) * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000.0 != 1000.1)
+# 3rd program
+print(2 * 2 + 2 == 2 * (2 + 2))
+# 4th program
+n = float('123.456')
+t = n * 10
+p = int(t)
+print(p % 10)
